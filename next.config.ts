41f6df297d6302b8a7595c69ade123c4a26caf6f
Julia Espinoza <juespinozacorrea@gmail.com>
2025-10-24 14:50:13 -0300

    Configuracion para build en Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    // 👇 Muy importante para que el binario/lib de Prisma se incluya en server chunks
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
 };
 
 export default nextConfig;
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,9 @@
+import 'dotenv/config';
+import path from 'node:path';
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: path.join('prisma', 'schema.prisma'),
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
